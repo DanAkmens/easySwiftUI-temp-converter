63535a9695585f28dc6c22a3f3c0131acfddbc57
Dan <Bdylan998@gmail.com>
2024-02-02 19:52:53 +0200

    Initial Commit

---
+++ temperature-conversion/temperature_conversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  temperature_conversionApp.swift
+//  temperature-conversion
+//
+//  Created by Dainis Putans on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct temperature_conversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
